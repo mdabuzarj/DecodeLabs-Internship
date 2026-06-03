152b90f778ff103897ef940cdddcc362e49d9263
mdabuzarj <mohammed.abuzar.j54@gmail.com>
2026-06-03 17:33:52 +0530

    Update GradeCalculator.java

--- Grade-Calculator/GradeCalculator.java
+++ Grade-Calculator/GradeCalculator.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class average {
+public class GradeCalculator {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int subjects = 0;
